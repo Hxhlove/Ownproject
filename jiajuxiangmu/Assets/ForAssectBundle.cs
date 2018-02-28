75f12c359a86c6d98a633f6b6e32a370c1be5647
Hxh <m18288731861@163.com>
2018-02-28 09:33:33 +0800

    Jiaju

---
+++ jiajuxiangmu/Assets/ForAssectBundle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class ForAssectBundle : MonoBehaviour 
+{
+	[MenuItem("Assets/AssectBundle/Build_IOS")]
+	public static void CreateAssetBunldesALL ()
+	{
+
+		Caching.CleanCache ();
+
+		string Path = Application.dataPath + "/StreamingAssets/ALL.assetbundle";
+
+		Object[] SelectedAsset = Selection.GetFiltered (typeof(Object), SelectionMode.DeepAssets);
+
+		foreach (Object obj in SelectedAsset) 
+		{
+			Debug.Log ("Create AssetBunldes name :" + obj);
+		}
+
+		//这里注意第二个参数就行
+		if (BuildPipeline.BuildAssetBundle (null, SelectedAsset, Path, BuildAssetBundleOptions.CollectDependencies,BuildTarget.iOS)) 
+		{
+			AssetDatabase.Refresh ();
+		} 
+		else {
+
+		}
+	}
+}
